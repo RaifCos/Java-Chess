506c5aa5062768bc83cfff065c38c4d77a597e56
RaifCos <128611497+RaifCos@users.noreply.github.com>
2024-09-10 00:20:53 +0100

    Update GameApplication.java
    = Gameplay Changes =
    • The "Castling" Technique is now possible in Gameplay.
    • Fixed a Bug where Pawns could "Jump Over" pieces on their first move. 
    
    = Visual Changes =
    • A new Visual Indicator besides the board shows whose turn it is.
    • A improved "Game Over" Screen. 
    
    = Code Changes =
    • Tidied Up Code
    • Added more Comments

--- JavaChess/src/GameApplication.java
+++ JavaChess/src/GameApplication.java
@@ -4,7 +4,9 @@ import javax.swing.*;
 import java.awt.image.*;
 import java.util.ArrayList;
 
-// TODO: Implement Bonus Tactics (Pawn Upgrades, Castling)
+// TODO: Fix Pawns (0,Y) Glitch
+// TODO: Implement "Check" Mode (Players can only make moves to protect their King).
+// TODO: Implement Pawn Upgrades
 
 public class GameApplication extends JFrame implements Runnable, MouseListener {
 
@@ -18,6 +20,10 @@ public class GameApplication extends JFrame implements Runnable, MouseListener {
     private Image boardImage;
     private Image backdropImage;
     private Image highlightImage;
+    private Image castlingImage;
+    private final Image backsplashImage;
+    private final Image whiteTurnImage;
+    private final Image blackTurnImage;
     private int boardIndent = 280;
 
     public int[][] boardData = new int[8][8];
@@ -47,12 +53,18 @@ public class GameApplication extends JFrame implements Runnable, MouseListener {
         // Add Input Listener.
         addMouseListener(this);
 
+        // Retrieve Images
         String workingDirectory = System.getProperty("user.dir");
         titleImage = (new ImageIcon(workingDirectory + "\\assets\\UI\\menuTitle.png")).getImage();
         backdropImage = (new ImageIcon(workingDirectory + "\\assets\\backdrops\\backdrop.png")).getImage();
         boardImage = (new ImageIcon(workingDirectory + "\\assets\\boards\\board.png")).getImage();
         highlightImage = (new ImageIcon(workingDirectory + "\\assets\\UI\\tileHighlight.png")).getImage();
+        castlingImage = (new ImageIcon(workingDirectory + "\\assets\\UI\\tileCastling.png")).getImage();
+        backsplashImage = (new ImageIcon(workingDirectory + "\\assets\\backdrops\\backsplash.png")).getImage();
+        whiteTurnImage = (new ImageIcon(workingDirectory + "\\assets\\UI\\turn0.png")).getImage();
+        blackTurnImage = (new ImageIcon(workingDirectory + "\\assets\\UI\\turn1.png")).getImage();
 
+        // Initialise Board Data
         boardData = new int[8][8];
 
         // Game can now be Initialised.
@@ -68,7 +80,7 @@ public class GameApplication extends JFrame implements Runnable, MouseListener {
         }
     }
 
-    // MouseListener Events
+    // MouseListener Event for when the Mouse is Clicked.
     public void mouseClicked(MouseEvent e) {
         int clickX = e.getX();
         int clickY = e.getY();
@@ -88,11 +100,13 @@ public class GameApplication extends JFrame implements Runnable, MouseListener {
         }
     }
 
+    // Other MouseListener Events
     public void mousePressed(MouseEvent e) {}
     public void mouseReleased(MouseEvent e) {}
     public void mouseEntered(MouseEvent e) {}
     public void mouseExited(MouseEvent e) {}
 
+    // Method to start the Game by assembling all Pieces on the board.
     public void startGame() {
         for(int i=0; i<8; i++) { whitePieces.add(new Pawn(0, i, 6)); }
         whitePieces.add(new Rook(0,0  ,7));
@@ -117,6 +131,7 @@ public class GameApplication extends JFrame implements Runnable, MouseListener {
         gameState = 1;
     }
 
+    // Refresh the State of each Piece and the Board.
     public void refreshBoardData() {
         selectedPiece = null;
         boardData = new int[8][8];
@@ -124,27 +139,40 @@ public class GameApplication extends JFrame implements Runnable, MouseListener {
         for (Piece p : blackPieces) { boardData[p.x][p.y] = 2; }
     }
 
+    // Method that handles the main Gameplay mechanics.
     public void pieceInteraction(int tileX, int tileY) {
-        // If there is a Piece Selected, and the Tile Clicked is a valid space, then move.
-        if (boardData[tileX][tileY] == 3 && selectedPiece != null) {
+        // If the King is selected and "Castling" is possible, then execute the move.
+        if (boardData[tileX][tileY] == 4 && selectedPiece instanceof King) {
+            castling(selectedPiece.team);
+            refreshBoardData();
+            whiteTurn = !whiteTurn;
+        } // If a Piece is selected and a valid Move Target is clicked, then move.
+        else if (boardData[tileX][tileY] == 3 && selectedPiece != null) {
             selectedPiece.move(tileX, tileY);
             capturePiece(tileX, tileY);
             refreshBoardData();
             whiteTurn = !whiteTurn;
-        } else {
+        } // No Piece is Selected, so check if the Player is clicking on a Piece to select it.
+        else {
+            // If it is White's Turn, check for a White Piece to select.
             if(whiteTurn) {
                 for (Piece p : whitePieces) {
                     if (p.x == tileX && p.y == tileY) {
                         selectedPiece = p;
                         boardData = selectedPiece.possibleMoves(boardData);
+                        // If a King is selected, check if "Castling" is possible.
+                        if(p instanceof King && p.moveCount == 0) { castleCheck(1); }
                         break;
                     } else { refreshBoardData(); }
                 }
-            } else {
+            } // If it is not White's Turn, check for a Black Piece to select.
+            else {
                 for (Piece p : blackPieces) {
                     if (p.x == tileX && p.y == tileY) {
                         selectedPiece = p;
                         boardData = selectedPiece.possibleMoves(boardData);
+                        // If a King is selected, check if "Castling" is possible.
+                        if(p instanceof King && p.moveCount == 0) { castleCheck(2); }
                         break;
                     } else { refreshBoardData(); }
                 }
@@ -152,19 +180,23 @@ public class GameApplication extends JFrame implements Runnable, MouseListener {
         }
     }
 
+    // Method that checks if a Moving Piece is "Capturing" another, removing it from the Game.
     public void capturePiece(int targetX, int targetY) {
+        // If it is not White's Turn, check for a White Piece to capture.
         if(!whiteTurn) {
             for (Piece p : whitePieces) {
                 if(p.x == targetX && p.y == targetY) {
                     whitePieces.remove(p);
+                    // If the removed Piece is the King, end the game.
                     if(p instanceof King) { checkmate(1); }
                     return;
                 }
-            }
+            } // If it is White's Turn, check for a Black Piece to capture.
         } else {
             for (Piece p : blackPieces) {
                 if(p.x == targetX && p.y == targetY) {
                     blackPieces.remove(p);
+                    // If the removed Piece is the King, end the game.
                     if(p instanceof King) { checkmate(0); }
                     return;
                 }
@@ -172,6 +204,7 @@ public class GameApplication extends JFrame implements Runnable, MouseListener {
         }
     }
 
+    // Method that ends the Game when a Player's King is captured.
     public void checkmate(int result) {
         switch (result) {
             case 0:
@@ -181,10 +214,38 @@ public class GameApplication extends JFrame implements Runnable, MouseListener {
                 winner = "Black Wins!";
                 break;
         }
-
         gameState = 2;
     }
 
+    // Method that carries out the "Castling" technique.
+    public void castling(int team) {
+        if (team == 1) {
+            for (Piece p : whitePieces) {
+                if(p instanceof King) { p.move(1, 7); }
+                if(p instanceof Rook && p.x == 0 && p.y == 7) { p.move(2, 7); }
+            }
+        } else if (team == 2) {
+            for (Piece p : blackPieces) {
+                if(p instanceof King) { p.move(6, 0); }
+                if(p instanceof Rook && p.x == 7 && p.y == 0) { p.move(5, 0); }
+            }
+        }
+    }
+
+    // Method to Check if the Player can execute the "Castling" technique.
+    public void castleCheck(int team) {
+        if (team == 1) {
+            for (Piece p : whitePieces) {
+                if (p instanceof Rook && p.moveCount == 0 && p.x == 0 && boardData[1][7] != 1 && boardData[2][7] != 1 && boardData[1][7] != 2 && boardData[2][7] != 2) { boardData[1][7] = 4; }
+            }
+        } else if (team == 2) {
+            for (Piece p : blackPieces) {
+                if (p instanceof Rook && p.moveCount == 0 && p.x == 7 && boardData[6][0] != 1 && boardData[5][0] != 1 && boardData[6][0] != 2 && boardData[5][0] != 2) { boardData[6][0] = 4; }
+            }
+        }
+    }
+
+    // Reset all Game Pieces and gameState.
     public void reset() {
         blackPieces.clear();
         whitePieces.clear();
@@ -197,17 +258,32 @@ public class GameApplication extends JFrame implements Runnable, MouseListener {
         if(!isInitialised) { return; }
         g = offscreenBuffer;
         g.setFont(new Font("Serif", Font.BOLD, 32));
+        g.setColor(Color.WHITE);
         switch (gameState) {
+            // Paint the Title Screen
             case 0:
                 g.drawImage(titleImage, 0, -25, null);
                 break;
+            // Paint the Board, Pieces, and Game UI.
             default:
                 g.drawImage(backdropImage, 0, -25, null);
                 g.drawImage(boardImage, boardIndent, 55, null);
+                if(whiteTurn) { g.drawImage(whiteTurnImage, 90, 330, null); }
+                else { g.drawImage(blackTurnImage, 90, 330, null); }
                 for (Piece p : whitePieces) { p.paint(g); }
                 for (Piece p : blackPieces) { p.paint(g); }
-                for (int i=0; i<8; i++) { for (int j=0; j<8; j++) { if(boardData[i][j] == 3) { g.drawImage(highlightImage, boardIndent+(80*i), 55+(80*j), null); } } }
-                if(gameState == 2) { g.drawString(winner, 515, 385); }
+                for (int i=0; i<8; i++) {
+                    for (int j=0; j<8; j++) {
+                        if(boardData[i][j] == 3) { g.drawImage(highlightImage, boardIndent+(80*i), 55+(80*j), null); }
+                        else if(boardData[i][j] == 4) { g.drawImage(castlingImage, boardIndent+(80*i), 55+(80*j), null); }
+                    }
+                }
+
+                // If Game is Over, Paint Results Screen.
+                if (gameState == 2) {
+                    g.drawImage(backsplashImage, 0, -25, null);
+                    g.drawString(winner, 515, 385);
+                }
                 break;
         }
         strategy.show();
