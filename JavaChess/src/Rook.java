b6641bf45e3ac3de6e92f62a453da03035e511e2
RaifCos <128611497+RaifCos@users.noreply.github.com>
2024-09-12 14:52:47 +0100

    Update Rook.java
    == Code Changes ==
    • The "Team" Variable has been standardised throughout the project.

--- JavaChess/src/Rook.java
+++ JavaChess/src/Rook.java
@@ -9,10 +9,10 @@ public class Rook extends Piece {
     }
 
     public int[][] possibleMoves(int[][] boardData) {
-        for (int z = x-1; z >= 0; z--) { if(boardData[z][y] != team) { if(boardData[z][y] == opp) { boardData[z][y] = 3; break; } else { boardData[z][y] = 3; } } else break; }
-        for (int z = x+1; z <= 7; z++) { if(boardData[z][y] != team) { if(boardData[z][y] == opp) { boardData[z][y] = 3; break; } else { boardData[z][y] = 3; } } else break; }
-        for (int z = y-1; z >= 0; z--) { if(boardData[x][z] != team) { if(boardData[x][z] == opp) { boardData[x][z] = 3; break; } else { boardData[x][z] = 3; } } else break; }
-        for (int z = y+1; z <= 7; z++) { if(boardData[x][z] != team) { if(boardData[x][z] == opp) { boardData[x][z] = 3; break; } else { boardData[x][z] = 3; } } else break; }
+        for (int z = x-1; z >= 0; z--) { if(boardData[z][y] != team + 1) { if(boardData[z][y] == opp + 1) { boardData[z][y] = 3; break; } else { boardData[z][y] = 3; } } else break; }
+        for (int z = x+1; z <= 7; z++) { if(boardData[z][y] != team + 1) { if(boardData[z][y] == opp + 1) { boardData[z][y] = 3; break; } else { boardData[z][y] = 3; } } else break; }
+        for (int z = y-1; z >= 0; z--) { if(boardData[x][z] != team + 1) { if(boardData[x][z] == opp + 1) { boardData[x][z] = 3; break; } else { boardData[x][z] = 3; } } else break; }
+        for (int z = y+1; z <= 7; z++) { if(boardData[x][z] != team + 1) { if(boardData[x][z] == opp + 1) { boardData[x][z] = 3; break; } else { boardData[x][z] = 3; } } else break; }
         return boardData;
     }
 
