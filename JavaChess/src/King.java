d763eafc9cf0a822ede06602eb2ae9b2503040fb
RaifCos <128611497+RaifCos@users.noreply.github.com>
2024-09-12 14:53:54 +0100

    Update King.java
    == Code Changes ==
    • The "Team" Variable has been standardised throughout the project.

--- JavaChess/src/King.java
+++ JavaChess/src/King.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class King extends Piece {
-
     public King(int team, int x, int y) {
         super(team, x, y);
         String workingDirectory = System.getProperty("user.dir");
@@ -11,11 +11,10 @@ public class King extends Piece {
     public int[][] possibleMoves(int[][] boardData) {
         for(int r = -1; r <= 1; r++ ) {
             for(int c = -1; c <= 1; c++ ) {
-                if(checkRange(x+r, y+c) && boardData[x+r][y+c] != team) { boardData[x+r][y+c] = 3; }
+                if(checkRange(x+r, y+c) && boardData[x+r][y+c] != team + 1) { boardData[x+r][y+c] = 3; }
             }
         }
         boardData[x][y] = 0;
         return boardData;
     }
-
 }
