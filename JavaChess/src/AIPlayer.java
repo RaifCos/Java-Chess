f22a173f0f2ddb098726b130adcaa5be46612e2b
RaifCos <128611497+RaifCos@users.noreply.github.com>
2024-09-24 16:31:57 +0100

    Update AIPlayer.java
    == Code Changes ==
    • Improved AI Opponent intelligence. It will now try to its less powerful pieces to capture the player's more valuable pieces.
    
    • Fixed a bug where after capturing it's first piece. the AI would freeze if it could not capture a piece.

--- JavaChess/src/AIPlayer.java
+++ JavaChess/src/AIPlayer.java
@@ -4,32 +4,33 @@ public class AIPlayer {
     Piece selectedPiece;
     BoardManager bm;
 
+    // Constructor
     public AIPlayer(BoardManager bm) { this.bm = bm; }
-
-    int[] res = new int[4];
+    int[] res;
 
     // Method to decide what move the AI will make on their turn.
     public int[] decideMove() {
         smartGen();
         if(selectedPiece != null) { return res; }
-
         // No possible moves found via smartGen, so randomly generate a move instead.
-        while (bm.boardData[res[2]][res[3]] != 3) {
-            bm.refreshBoardData();
-            randomGen();
-        } return res;
+        randomGen();
+        for(int i=0; i<4; i++){ System.out.println(res[i]); }
+        System.out.println("\n");
+        return res;
     }
 
     // Method to search the board for the best move.
     public void smartGen() {
-        int maxScore = 0;
+        res = new int[4];
+        selectedPiece = null;
+        int maxScore = -900;
         bm.setBoardScore();
         for (Piece p : bm.getPieceList(1)) {
             bm.refreshBoardData();
             p.possibleMoves(bm.boardData);
             for (int i = 0; i < 8; i++) {
                 for (int j = 0; j < 8; j++) {
-                    if(bm.boardData[i][j] == 3 && bm.scoreData[i][j] > maxScore) {
+                    if(bm.boardData[i][j] == 3 && bm.scoreData[i][j] > 0 && (bm.scoreData[i][j] - p.score) > maxScore) {
                         maxScore = bm.scoreData[i][j];
                         selectedPiece = p;
                         res[0] = p.x;
@@ -44,11 +45,15 @@ public class AIPlayer {
 
     // Method to randomly generate a space on the Board.
     public void randomGen() {
-        Piece p = bm.getPieceList(1).get((int)(Math.random() * bm.getPieceList(1).size()));
-        p.possibleMoves(bm.boardData);
-        res[0] = p.x;
-        res[1] = p.y;
-        res[2] = (int)(Math.random() * 8);
-        res[3] = (int)(Math.random() * 8);
+        while (bm.boardData[res[2]][res[3]] != 3) {
+            bm.refreshBoardData();
+            res = new int[4];
+            Piece p = bm.getPieceList(1).get((int)(Math.random() * bm.getPieceList(1).size()));
+            p.possibleMoves(bm.boardData);
+            res[0] = p.x;
+            res[1] = p.y;
+            res[2] = (int)(Math.random() * 8);
+            res[3] = (int)(Math.random() * 8);
+        }
     }
 }
