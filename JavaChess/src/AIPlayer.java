3d845b50fb04d28f1a174a1e5da992a390002077
RaifCos <128611497+RaifCos@users.noreply.github.com>
2024-09-24 12:26:00 +0100

    Update AIPlayer.java
    == Gameplay Changes ==
    • AI Opponent is now more intelligent, assessing the board each turn for the best capture.

--- JavaChess/src/AIPlayer.java
+++ JavaChess/src/AIPlayer.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public class AIPlayer {
+    Piece selectedPiece;
     BoardManager bm;
 
     public AIPlayer(BoardManager bm) { this.bm = bm; }
@@ -9,11 +10,36 @@ public class AIPlayer {
 
     // Method to decide what move the AI will make on their turn.
     public int[] decideMove() {
+        smartGen();
+        if(selectedPiece != null) { return res; }
+
+        // No possible moves found via smartGen, so randomly generate a move instead.
         while (bm.boardData[res[2]][res[3]] != 3) {
             bm.refreshBoardData();
             randomGen();
+        } return res;
+    }
+
+    // Method to search the board for the best move.
+    public void smartGen() {
+        int maxScore = 0;
+        bm.setBoardScore();
+        for (Piece p : bm.getPieceList(1)) {
+            bm.refreshBoardData();
+            p.possibleMoves(bm.boardData);
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if(bm.boardData[i][j] == 3 && bm.scoreData[i][j] > maxScore) {
+                        maxScore = bm.scoreData[i][j];
+                        selectedPiece = p;
+                        res[0] = p.x;
+                        res[1] = p.y;
+                        res[2] = i;
+                        res[3] = j;
+                    }
+                }
+            }
         }
-        return res;
     }
 
     // Method to randomly generate a space on the Board.
